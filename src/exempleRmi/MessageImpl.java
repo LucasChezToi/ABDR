0bcdb8dc8300e57a0f7729cae3646abae3fca1de
LucasChezToi <lucas.simonet@gmail.com>
2014-01-05 21:37:25 +0100

    eh tyjentyndtgnzsrt  serthz rth erth zergh

---
+++ src/exempleRmi/MessageImpl.java
@@ -0,0 +1,14 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class MessageImpl extends UnicastRemoteObject implements Msg {
+
+    public MessageImpl() throws RemoteException {        
+    }
+    
+    @Override
+    public void sayHello(String name) throws RemoteException {
+        System.out.println("hello "+name);
+    }
+    
+}
---
+++ src/exempleRmi/Msg.java
@@ -0,0 +1,6 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Msg extends Remote {
+    void sayHello(String name) throws RemoteException;
+}
